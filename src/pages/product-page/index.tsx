a00b720f8a7d436b3a243cf0fdfd34a2b460e43c
Youssef Outahar <youssefoutahar1@gmail.com>
2024-01-18 22:43:23 +0100

    Refactor product page component and add loading state

--- src/pages/product-page/index.tsx
+++ src/pages/product-page/index.tsx
@@ -7,45 +7,62 @@ import toast from "react-hot-toast";
 const ProductPage = () => {
     const { id } = useParams();
     const [produit, setProduit] = useState<Produit>();
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
 
     const fetchProduct = async () => {
-        toast.promise(axios.get<Produit>("https://fakestoreapi.com/products/" + id), {
-            loading: "Loading product...",
-            success: (res) => {
-                setProduit(res.data);
-                return "Product loaded";
-            },
-            error: () => {
-                setError(`An error occurred`);
-                return "Error while loading product";
-            },
-        });
+        setLoading(true);
+        try {
+            const response = await axios.get<Produit>("https://fakestoreapi.com/products/" + id);
+            setProduit(response.data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(`An error occurred`);
+        }
     };
 
     useEffect(() => {
         fetchProduct();
     }, []);
 
+    const handleBuy = () => {
+        toast.success("Produit acheté");
+    };
+
     return (
         <>
-            ¨
-            {error ? (
+            {loading ? (
+                <div className="flex justify-center items-center min-h-screen">
+                    <div className="text-black text-center font-bold text-xl">
+                        <p>Loading...</p>
+                    </div>
+                </div>
+            ) : error ? (
                 <div className="text-red-800 font-extrabold">{error}</div>
             ) : (
-                <div className="flex flex-col space-y-2 bg-gray-50 p-4">
-                    <img src={produit?.image} className="w-full h-56 object-contain rounded-md mb-4" />
-                    <div className="text-xl h-32 text-gray-800 text-center font-extrabold">
-                        {produit?.title}
-                    </div>
-
-                    <div className="text-md text-gray-800 font-extrabold">{produit?.category}</div>
-                    <div className="text-md text-gray-800 font-extrabold">{produit?.description}</div>
-
-                    <div className="flex items-center justify-between">
-                        <div className="text-2xl text-orange-500 font-extrabold">{produit?.price}€</div>
-                        <div className="text-md text-blue-800 font-extrabold">
-                            avis : {produit?.rating.rate} / 5
+                <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+                    <div className="flex flex-col-reverse justify-start gap-8 sm:gap-20 lg:flex-row">
+                        <img src={produit?.image} alt="" className="inline-block max-w-sm" />
+                        <div className="flex flex-col items-start lg:justify-center">
+                            <div className="text-xl h-32 text-gray-800 text-center font-extrabold">
+                                {produit?.title}
+                            </div>
+                            <p className="mb-6 max-w text-black md:mb-12">{produit?.description}</p>
+                            <div className="flex items-center justify-between w-full mb-5">
+                                <div className="text-2xl text-orange-500 font-extrabold">
+                                    {produit?.price}€
+                                </div>
+                                <div className="text-md text-blue-800 font-extrabold">
+                                    avis : {produit?.rating.rate} / 5
+                                </div>
+                            </div>
+                            <button
+                                onClick={handleBuy}
+                                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                            >
+                                <span>Buy</span>
+                            </button>
                         </div>
                     </div>
                 </div>
